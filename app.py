9de53e76c9e3d7c0aab5a213fc9fa548e1b2a7cc
Sreenivassreeee <ksreenivascse@gmail.com>
2024-03-12 16:02:28 +0530

    archive

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
-from coremltools import converters
-from coremltools.models import MLModel
-from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import coremltools
+from coremltools.models import MLModel
 
-# Load the CSV data
+
 data = pd.read_csv("twitter-sanders-apple3.csv")
-
-# Split the data into training and testing sets
 training_data, testing_data = train_test_split(data, test_size=0.2, random_state=5)
 
-# Create an MLTextClassifier
-sent_classifier = converters.sklearn.convert(training_data, target="class", features=["text"])
 
-# Evaluate the model
-evaluation_metrics = sent_classifier.evaluate(testing_data, target="class", features=["text"])
-accuracy = (1.0 - evaluation_metrics["classification_error"]) * 100
 
-# Save the model
-metadata = {
-    "author": "Sreenivas",
-    "short_description": "This is trained sentiment classifier",
-    "version": "1.0"
-}
-mlmodel = MLModel(sent_classifier, metadata=metadata)
-mlmodel.save("TweetClassifier.mlmodel")
+tfidf_vectorizer = TfidfVectorizer()
+X_train = tfidf_vectorizer.fit_transform(data["text"])
+y_train = data["class"]
 
-# Make predictions
-prediction1 = mlmodel.predict({"text": "@apple is a waste company"})
-prediction2 = mlmodel.predict({"text": "@apple is love"})
+model_pipeline = Pipeline([
+    ('scaler', StandardScaler(with_mean=False)),
+    ('classifier', LogisticRegression(multi_class='ovr'))
+])
+
+model_pipeline.fit(X_train, y_train)
+
+model_pipeline.multi_class = 'ovr'
+coreml_model = coremltools.converters.sklearn.convert(model_pipeline)
+
+try:
+    coreml_model = MLModel("TweetClassifier.mlmodel") 
+    print("----------- Already saved ---------------")
+except:
+    coreml_model.save("TweetClassifier.mlmodel")
+    print("----------- Saved ---------------")
+
+
+
+
+
+for i in range(0,100):
+    input_text =input("Sentence : ")
+    input_features = tfidf_vectorizer.transform([str(input_text)])
+    input_features_dense = input_features.toarray()
+    prediction = coreml_model.predict({"input": input_features_dense})
+    print(prediction)
+
+
